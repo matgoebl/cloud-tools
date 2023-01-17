1729055ed84fbc070d91a5b0a1bd9df62c034a4c
Matthias Goebl <matthias.goebl@goebl.net>
2023-01-17 07:04:54 +0100

    fix example

--- src/avro.codec-plugin.py
+++ src/avro.codec-plugin.py
@@ -16,16 +16,16 @@ class AvroPlugin(IPlugin):
 
         # another example, where the schema name is prepended to the real payload:
         #schema_name_len = raw_payload[0]
-        #schema_name = raw_payload[1:schema_name_len+1]
+        #schema_name = raw_payload[1:schema_name_len+1].decode()
         #raw_payload = raw_payload[schema_name_len+1:]
 
-        # Lazy load schemas once when they are used first:
+        # Lazy load schemas once on their first use:
         if not schema_name in self.schemas:
             try:
                 logging.debug(f"Loading AVRO schema {schema_name}...")
                 self.schemas[schema_name] = fastavro.schema.load_schema( os.path.join(self.schemas_path, schema_name + ".avsc"))
             except:
-                logging.warn(f"Cannot load AVRO schema for {schema_name}...")
+                logging.warn(f"Cannot load AVRO schema for {schema_name}")
                 self.schemas[schema_name] = None
 
         if not self.schemas[schema_name]:
