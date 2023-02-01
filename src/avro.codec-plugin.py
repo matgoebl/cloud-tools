98f1b5a6bec91bf019da6e3a0f7b1dad8e2aec4e
Matthias Goebl <matthias.goebl@goebl.net>
2023-02-01 22:42:17 +0100

    add decoder for AWS Glue Schema Registry Java Client

--- src/avro.codec-plugin.py
+++ src/avro.codec-plugin.py
@@ -3,6 +3,8 @@ import os
 import logging
 import fastavro
 import io
+import zlib
+import uuid
 
 class AvroPlugin(IPlugin):
     def __init__(self):
@@ -18,6 +20,22 @@ class AvroPlugin(IPlugin):
         #schema_name_len = raw_payload[0]
         #schema_name = raw_payload[1:schema_name_len+1].decode()
         #raw_payload = raw_payload[schema_name_len+1:]
+
+        # for using the AWS Glue Schema Registry with the Java Client:
+        # the encoding is implemented here: https://github.com/awslabs/aws-glue-schema-registry/blob/master/serializer-deserializer/src/main/java/com/amazonaws/services/schemaregistry/serializers/SerializationDataEncoder.java
+        # the constants can be found here: https://github.com/awslabs/aws-glue-schema-registry/blob/master/common/src/main/java/com/amazonaws/services/schemaregistry/utils/AWSSchemaRegistryConstants.java
+        # inspired also from: https://github.com/DisasterAWARE/aws-glue-schema-registry-python/blob/main/src/aws_schema_registry/codec.py
+        header_version_byte = int(raw_payload[0])
+        compression_type_byte = int(raw_payload[1])
+        schema_version_id = str(uuid.UUID(bytes=raw_payload[2:18]))
+        HEADER_VERSION_GLUE = 3
+        COMPRESSION_TYPE_ZLIB = 5
+        if header_version_byte == HEADER_VERSION_GLUE:
+            if compression_type_byte == COMPRESSION_TYPE_ZLIB:
+                raw_payload = zlib.decompress(raw_payload[18:])
+            else:
+                raw_payload = raw_payload[18:]
+            schema_name = topic + "." + schema_version_id
 
         # Lazy load schemas once on their first use:
         if not schema_name in self.schemas:
